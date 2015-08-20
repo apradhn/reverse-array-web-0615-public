a9e4c2ff8c9c14d4351b7b5f3f807db58e6655b6
apradhn <abhijit.pradhan6@gmail.com>
2015-08-19 21:23:42 -0400

    refactors solution to use comma separated assignment

--- reverse_array.rb
+++ reverse_array.rb
@@ -3,10 +3,7 @@ class Array
     self.size.even? ? right_idx = self.size / 2 : right_idx = (self.size / 2) + 1
     left_idx = (self.size / 2) - 1
     loop do 
-      right_val = self[right_idx]
-      left_val = self[left_idx]
-      self[right_idx] = left_val
-      self[left_idx] = right_val
+      self[right_idx], self[left_idx] = self[left_idx], self[right_idx]
       break if left_idx == 0
       right_idx += 1
       left_idx -= 1 
