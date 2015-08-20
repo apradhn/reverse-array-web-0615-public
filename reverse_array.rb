ec0c8cd63ba1f479b9ceeee38280926abbfe3329
apradhn <abhijit.pradhan6@gmail.com>
2015-08-19 21:03:40 -0400

    completes challenge

--- reverse_array.rb
+++ reverse_array.rb
@@ -1,1 +1,16 @@
-# your code here
+class Array
+  def my_reverse
+    self.size.even? ? right_idx = self.size / 2 : right_idx = (self.size / 2) + 1
+    left_idx = (self.size / 2) - 1
+    loop do 
+      right_val = self[right_idx]
+      left_val = self[left_idx]
+      self[right_idx] = left_val
+      self[left_idx] = right_val
+      break if left_idx == 0
+      right_idx += 1
+      left_idx -= 1 
+    end
+    self
+  end
+end
